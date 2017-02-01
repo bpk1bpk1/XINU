b0f50e348e5c79b55d3d26e08896950b28896509
Prerana <bkamath@umail.iu.edu>
2017-02-01 18:51:28 -0500

    Queue with every function working :)

--- programs/assignment_3.c
+++ programs/assignment_3.c
@@ -14,10 +14,12 @@ struct node *current = NULL;
 struct node *rear = NULL;
 struct node *front = NULL;
 
+
 // Various functions of the queue
 void enqueue(int id, int value);
 struct node* dequeue();
 void display();
+void priority_enqueue(int ids,int values);
 
 //Main Function
 void main()
@@ -29,20 +31,21 @@ void main()
 	printf(" Please choose one of the following options");
 	printf(" \n 1. Enqueue - Enter in the queue ");
 	printf(" \n 2. Dequeue - Delete from the queue");
-	printf(" \n 3. Display - Show the contents of the queue");
-	printf(" \n 4. Exit - Exit the program");
+	printf(" \n 3. Priority Enqueue - Enter into the queue with the key vales as priority");
+	printf(" \n 4. Display - Show the contents of the queue");
+	printf(" \n 5. Exit - Exit the program");
 
 
 	while(1)
 	{
-		printf("\n Enter one of the options from the menu");
+		printf("\n Enter one of the options from the menu: ");
 		scanf("%d",&choice);
 		switch(choice)
 		{
 			case 1:
-				printf("\n Enter the processid");
+				printf("\n Enter the processid: ");
 				scanf(" %d", &processid);
-				printf("\n Enter the keyvalue");
+				printf("\n Enter the keyvalue: ");
 				scanf(" %d", &keyvalue);
 				enqueue(processid,keyvalue);
 				break;
@@ -50,24 +53,81 @@ void main()
 				delete = dequeue();
 				if(delete == NULL)
 				{
-					printf("Sorry the queue is empty");
+					printf("Sorry! The queue is empty!");
 				}
 				else
 				{
-				printf("The deleted element has a process id %d", delete -> pid);
-				printf("The deleted element has a key value %d", delete -> key);
+				printf("The deleted element has a process id:  %d", delete -> pid);
+				printf("\n The deleted element has a key value:  %d", delete -> key);
 				}
 				break;
 			case 3:
-				display();
+				printf("\n Enter the processid: ");
+				scanf(" %d", &processid);
+				printf(" \n Enter the priority key value: ");
+				scanf(" %d", &keyvalue);
+				priority_enqueue(processid,keyvalue);
 				break;
 			case 4:
+				display();
+				break;
+			case 5:
 				exit(0);
 			default:
 				printf("\n Please enter the correct choice from the menu!");
 				break;
 		}
 		}
+}
+
+//Priority enqueue 
+void priority_enqueue(int ids,int values)
+{
+    struct node *find  = front;
+	struct node *link1 = (struct node*) malloc (sizeof(struct node));
+	link1 -> pid = ids;
+	link1 -> key = values;
+	// No node in the queue
+	if(front == NULL)
+	{
+		front = rear = link1;
+	}
+	else
+	{ 
+		//Most priority
+		if(link1 -> key > find -> key)
+		{
+			front -> prev = link1;
+			link1 -> next = front;
+			front = link1;
+		}
+
+		//Looking for the position to place the node according to priority
+		else
+		{
+			while(link1 -> key < find -> key)
+			{
+				if(find -> next == NULL)
+				break;
+				find = find -> next;
+			}
+
+			if(find -> next == NULL && link1 -> key < find -> key)
+			{
+				rear -> next = link1;
+				link1 -> prev = rear;
+				rear = link1;
+			}
+
+			else
+			{
+				find -> next -> prev = link1;
+				link1 -> prev = find -> prev;
+				link1 -> next = find;
+				find -> prev = link1;
+			}
+		}
+	}
 }
 
 
@@ -95,24 +155,36 @@ void enqueue(int id, int value)
 //Deleting from the beginning (FIFO Xinu requirements)
 struct node* dequeue()
 {
-	struct node *temp = front;
+	struct node *temp;
 
-	//Only one node left in the queue
-	if(front -> next == NULL)
+	//No  node left in the queue
+	if(front == NULL)
 	{
-		rear = NULL;
 		temp = NULL;
+		return temp;
 		
+	}
+	//Only one node left in the queue
+	else if(front -> next == NULL)
+	{
+		struct node* tempo = front;
+		temp = tempo;
+		front = NULL;
+		rear  = NULL;
+		return temp;
 	}
 	else
 	{
-		front -> next -> prev = NULL;
+		struct node* tempo1 = front;
+		temp = tempo1;
+		front = front -> next;
+		front -> prev = NULL;
+
+		return temp;
 	}
 
-	front = front -> next;
 
 	//returning the deleted element
-	return temp;
 }
 		
 

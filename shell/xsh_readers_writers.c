fa191396455649efc636c9c8881041872174e101
bohenderson <bohende@gmail.com>
2017-02-14 19:28:37 -0500

    finished reader/writer

--- shell/xsh_readers_writers.c
+++ shell/xsh_readers_writers.c
@@ -6,9 +6,21 @@
 
 #define MAXPROC 10
 #define MAXCYCLES 10
-#define MAXMAXDELAY 5000
+#define MAXMAXDELAY 10000
 #define MINALL 1
 #define REQNARGS 6
+
+void reader(int ri, int rc);
+void writer(int wi, int wc);
+
+int nkids;
+int nreaders;
+int maxdelay; // in miliseconds
+
+sid32 roomEmpty;
+sid32 mutex;
+sid32 turnstile;
+sid32 isdone;
 
 /*------------------------------------------------------------------------
  * xsh_date - obtain and print the current month, day, year, and time
@@ -16,19 +28,20 @@
  */
 shellcmd xsh_readers_writers(int nargs, char *args[]) {
 
+  nreaders = 0;
+
   // input variable declarations
   int nWriters;
   int nReaders;
   int nWriteCycles;
   int nReadCycles;
-  int maxdelay; // in miliseconds
 
   // make sure the correct number of arguments were passed or set sensible defaults
   if (nargs == 1) {
     nWriters = 3;
     nReaders = 3;
-    nWriteCycles = 5;
-    nReadCycles = 5;
+    nWriteCycles = 3;
+    nReadCycles = 3;
     maxdelay = 1000;
   } else if (nargs > REQNARGS) {
     kprintf("Error, too many arguments. Expected %d.\n", REQNARGS-1); return 1;
@@ -62,7 +75,69 @@ shellcmd xsh_readers_writers(int nargs, char *args[]) {
     }  
   }
 
-  kprintf("Writers: %d\tReaders: %d\tWrite Cycles: %d\tRead Cycles: %d\tMax Delay: %d\n", nWriters, nReaders, nWriteCycles, nReadCycles, maxdelay);
+  kprintf("Writers: %d\tReaders: %d\tWrite Cycles: %d\tRead Cycles: %d\tMax Delay: %d\n\n", nWriters, nReaders, nWriteCycles, nReadCycles, maxdelay);
 
+  // initialize semophores
+  roomEmpty = semcreate(1);
+  mutex = semcreate(1);
+  turnstile = semcreate(1);
+  nkids = nWriters + nReaders;
+
+  for ( ; nWriters > 0; nWriters--) {
+    resume(create(writer, 1024, 20, "writer", 2, nWriters, nWriteCycles));
+  }
+
+  for ( ; nReaders > 0; nReaders--) {
+    resume(create(reader, 1024, 20, "reader", 2, nReaders, nReadCycles));
+  }
+
+  chprio(getpid(), 19); // lower this processes' priority to minimize busywaiting
+  while(nkids>0) { sleep(0.5); } // wait until the number of child processes running is zero
   return 0;
 }
+
+
+void reader(int ri, int rc) {
+
+  for ( ; rc >= 0; rc--) {
+    // pass through the turnstile
+    wait(turnstile);
+    signal(turnstile);
+
+    // increment the number of readers one at a time
+    wait(mutex);
+    if (++nreaders == 1) {
+      wait(roomEmpty); // first reader in needs to wait until the room is empty
+    }
+    signal(mutex);
+
+    rand_delay(maxdelay); // Do our reading task for a random amount of time
+    kprintf("Reader %d\t%d\n", ri, rc);
+
+    // decrement the number of readers one at a time
+    wait(mutex);
+    if (--nreaders == 0) {
+      signal(roomEmpty); // last reader out needs to signal that the room is empty
+    }
+    signal(mutex);
+  }
+  nkids--;
+  return;
+}
+
+void writer(int wi, int wc) {
+  for ( ; wc >= 0; wc--) {
+    
+    wait(turnstile); // try to pass through the turnstile
+    wait(roomEmpty); // block readers if the room isn't empty
+
+    rand_delay(maxdelay); // do our writing task for some random amount of time
+    kprintf("Writer %d\t%d\n", wi, wc);
+
+    signal(turnstile); // finish passing throug the turnstile
+    signal(roomEmpty); // the room is empty when a writer leaves
+  }
+  nkids--;
+  return;
+}
+
